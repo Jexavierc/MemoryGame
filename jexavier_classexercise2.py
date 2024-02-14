29a0e304b7a54245997025c0f6089a648d7cb3c1
jexavier clemente <jexavierc@gmail.com>
2024-02-14 14:03:31 -0500

    inital commit

---
+++ jexavier_classexercise2.py
@@ -0,0 +1,28 @@
+#Author: Jexavier
+#Date: 2/7/2024
+#Desc: Class exercise 2 (math formulas)
+
+#1 given the two points p1 and p2, calculate the distance between them.
+
+p1 = (3,5)
+p2 = (6,7)
+
+x1, y1 = p1[0], p1[1]
+x2, y2 = p2[0], p2[1]
+
+distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print('the distance = ' , distance)
+
+
+
+######
+
+x = 7
+b=2
+m=1.33
+
+y = m*x + b
+
+print('y = ',y)
+
+
