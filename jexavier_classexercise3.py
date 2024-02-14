23d6779c3572f4b3f78df4a7ed01a5c02df84cea
jexavier clemente <jexavierc@gmail.com>
2024-02-14 14:07:11 -0500

    inital commit

---
+++ jexavier_classexercise3.py
@@ -0,0 +1,29 @@
+import turtle
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.fillcolor('red')
+
+# move turtle to create the next square 
+turtle.goto(100, 0)
+
+
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
+turtle.up()
