98a2c2da8b8d3a04959ea4edc3e3f34bf2ae2759
jexavier clemente <jexavierc@gmail.com>
2024-02-14 14:09:52 -0500

    inital commit

---
+++ jexavier_ex2.py
@@ -0,0 +1,13 @@
+# class ex 2
+# find the average fo grades
+#grade = 67, 48, 91, 77, 80
+
+total = 0 
+for num in range(5):
+    grade - int(input("please enter grade"))
+    total = total + grade
+
+average = total / 5
+
+
+print('The Avg = ', average)
