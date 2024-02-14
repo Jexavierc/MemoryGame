0abd75f9a6b0ce8e1a7b96789611b82e8402311e
jexavier clemente <jexavierc@gmail.com>
2024-02-14 14:02:00 -0500

    Initial commit

---
+++ jexavier_classexercise1.py
@@ -0,0 +1,21 @@
+#Author: Jexavier
+#Date: 2/7/2024
+#Desc: Class exercise 1 (math formulas)
+
+#1. Convert cel to faren
+cel = 20
+faren = ((cel *9) / 5) + 32
+print(cel,' 20 = ', faren, '68'
+
+#2. Convert lbs to kilos
+pounds = 40
+kilo = 40 * 0.454 
+print(pounds, ' 40 = ' ,kilo, '18.16'
+
+#3. convert inches to centimeters
+inches = 12
+centimeters = 12 * 2.54
+print(icnhes, ' 12 = ' ,centimeters, '30.48' 
+      
+
+      
