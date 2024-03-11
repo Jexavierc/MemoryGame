087da6df22ebb0a6b0d60ae1c674904c4b1abe4c
jexavier clemente <jexavierc@gmail.com>
2024-03-11 14:33:01 -0400

    new code

---
+++ ProgrammingExcercise2-12.py
@@ -0,0 +1,9 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Springboard/AppData/Local/Programs/Python/Python312/ProgrammingExcercise2-12.py
+Amount paid for the stock: $ 80000.00
+Commission paid on the purchase: $ 2400.00
+Amount the stock sold for $ 85500.00
+Commission paid on the sale: $ 2565.00
+Profit ( or loss if negative) : $ 535.00
---
+++ ProgrammingExcercise2-5.py
@@ -0,0 +1,15 @@
+distance6Hours = 0.0
+distance10Hours = 0.0
+disance15Hours = 0.0
+
+
+
+SPEED = 70
+
+distance6Hours = SPEED*6
+distance10Hours = SPEED*10 
+distance15Hours = SPEED*15
+print ("the car will travel the following distance:")
+print (distance6Hours,",miles in 6 hours.")
+print (distance10Hours,"miles in 10 hours.")
+print (distance15Hours,"miles in 15 hours.")
